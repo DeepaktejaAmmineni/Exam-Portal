fabfd4b9c4f830a2d3fe543b416e5dd901331a82
Deepak Teja Ammineni <deepakteja34@gmail.com>
2026-03-16 17:14:34 +0530

    Update API URL to Railway backend

--- exam_portal_frontend/src/utils/api.js
+++ exam_portal_frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://exam-portal-production-37cb.up.railway.app',
 });
 
 // Attach JWT token to every request automatically
